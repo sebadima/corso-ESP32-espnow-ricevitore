191167b791b4685c559a1f5fe68f225d8f077976
Your Name <you@example.com>
2024-03-02 18:02:54 +0100

    fix skel

--- src/main.ino
+++ src/main.ino
@@ -1,7 +1,7 @@
 #include "ESPAsyncWebServer.h"
 #include <WiFi.h>
 
-constexpr char WIFI_SSID[] = "D-Link-3D1BBF";
+constexpr char WIFI_SSID[] = "D-Link-3D1BBF000";
 constexpr char WIFI_PASS[] = "p2mxsajrqwkd9c7g";
 
 IPAddress local_IP(192, 168, 1, 200);
@@ -23,22 +23,22 @@ const char index_html[] PROGMEM = R"rawliteral(
 </html>)rawliteral";
 
 void initWiFi() {
-    WiFi.mode(WIFI_MODE_APSTA);
+    WiFi.mode(WIFI_MODE_STA);
 
     if(!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
-      Serial.println("STA Failed to configure");
+      Serial.println("Non riesco a configurare la modalità station (STA)");
     }
 
     WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-    Serial.printf("Connecting to %s .", WIFI_SSID);
+    Serial.printf("In connessione a %s .", WIFI_SSID);
     while (WiFi.status() != WL_CONNECTED) { Serial.print("."); delay(200); }
-    Serial.println("ok");
+    Serial.println("connesso!");
 
     IPAddress ip = WiFi.localIP();
 
     Serial.printf("SSID: %s\n", WIFI_SSID);
-    Serial.printf("Channel: %u\n", WiFi.channel());
+    Serial.printf("Canale: %u\n", WiFi.channel());
     Serial.printf("IP: %u.%u.%u.%u\n", ip & 0xff, (ip >> 8) & 0xff, (ip >> 16) & 0xff, ip >> 24);
 }
 
